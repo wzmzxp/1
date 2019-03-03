9fa1a2e21ad37a8d5daca6ae97c7263461211efd
wzmzxp <2581806204@qq.com>
2019-03-03 17:24:33 +0800

    9999

---
+++ git/5-200/Untitled.m
@@ -0,0 +1,2 @@
+global s citaf citar citam;
+s=0.2;citaf=38.6;citar=-15.4;citam=16.675;
--- git/5-200/obj.m
+++ git/5-200/obj.m
@@ -1,7 +1,9 @@
-function [ output_args ] = obj( input_args )
-%OBJ 此处显示有关此函数的摘要
-%   此处显示详细说明
-
-
+function [ output_args ] = obj( ganma )
+global s citaf citar citam;
+delta=0;j=0;ksi=0;
+a1=0.16*ganma;
+% a2=(4140+delta*tan(13))*(1-exp(-100*j))*cos(ksi)-de;ta*sin(ksi);
+% a3=0.055*power(cos())
+a1*quad(@obi2,citar,citaf);
 end
 
