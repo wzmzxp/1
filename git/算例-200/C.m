50bdf56daa6ee16ee69d89b24b35d3dfe9ca35f6
wzmzxp <2581806204@qq.com>
2019-03-07 14:21:40 +0800

    hh

--- git/算例-200/C.m
+++ git/算例-200/C.m
@@ -1,13 +1,13 @@
 function [ output_args ] = C( x,y,z,t )
 %C 此处显示有关此函数的摘要
 %   此处显示详细说明
-Q=6.5*1e+12;u=6;
-a1=Q/(power(2*pi,3/2)*sigmax(x)*sigmay(x)*sigmaz(x));
- a2=power((x-u*t),2)/power(sigmax(x),2)+(y*y)/power(sigmay(y),2);
- a3=exp(-0.5*a2);
+Q=8000;u=9.64;
+a1=Q/((power(2*pi,3/2)*sigmax(x)*sigmay(x)*sigmaz(x))+eps);
+ a2=(power((x-u*t),2)+eps)/(power(sigmax(x),2)+eps)+(y*y)/(power(sigmay(y),2)+eps);
+ a3=exp(-1*a2);
 % a2=exp(-0.5*(power((x-u*t),2)/power(sigmax(x),2)+(y*y)/(sigmay(y)*sigmay(y))));
 
-a4=exp(-0.5*(power((z+H(t)),2)/power(sigmaz(z),2)+power((z-H(t)),2)/power(sigmaz(z),2)));
-output_args=a1*a3*a4;
+a4=exp(-1*((power((z+H(t)),2)+eps)/(power(sigmaz(z),2)+eps)+(power((z-H(t)),2)+eps)/(power(sigmaz(z),2)+eps)));
+output_args=(a1+eps)*(a3+eps)*(a4+eps);
 end
 
