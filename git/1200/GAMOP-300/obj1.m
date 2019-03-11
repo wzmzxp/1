bae8bc7acc5fab8849ec18084c87390b1332d32b
wzmzxp <2581806204@qq.com>
2019-03-11 20:34:06 +0800

    嘎嘎嘎嘎嘎过过过过

---
+++ git/1200/GAMOP-300/obj1.m
@@ -0,0 +1,23 @@
+function [outputArg1] = obj1(h1,up1)
+global N h up Ck Cp Cf
+h=h1;up=up1;
+% 
+% fenzi=0;
+% fenmu=0;
+% for k=1:N
+%     a1=(k-1)*Ck+Cp;
+%     Ppk1=Ppk(k*h+up,k*h);
+%     a2=(k-1)*Ck+Cf;
+%     Pfk1=Pfk(k*h,(k-1)*h);
+%     fenzi=fenzi+a1*Ppk1+a2*Pfk1;
+%     a3=E(h,k)*Pfk1;
+%     a4=k*h*Ppk1;
+%     fenmu=fenmu+a3+a4;
+%    
+% end
+%  outputArg1=fenzi/fenmu;
+a=Ec(h1,up1)
+% b=Et(h1,up1);
+outputArg1=a;
+end
+
